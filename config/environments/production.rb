7c88bec260ad0949f9761a725c050061edc18ca7
Jai Rajput <loveyhtu21@gmail.com>
2015-12-07 15:53:46 +0530

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ SLOtility::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = {host: 'SLOtility.herokuapp.com', protocol: 'http://' }
-
+  config.action_mailer.default_url_options = {host: '', protocol: 'http://' }
+                                                    # SLOtility.herokuapp.com
   #Mailer Code
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
